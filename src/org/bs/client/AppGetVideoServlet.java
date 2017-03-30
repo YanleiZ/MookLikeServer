3a0e2132a629e9e6a4c628c6d43f9fdda5025aa7
yanleiz <yanlei_man@outlook.com>
2017-03-30 17:17:01 +0800

    test

--- src/org/bs/client/AppGetVideoServlet.java
+++ src/org/bs/client/AppGetVideoServlet.java
@@ -22,6 +22,7 @@ public class AppGetVideoServlet extends BaseServlet {
 		super.get(request, response);
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
+		System.out.println("===");
 		
 	}
 	public void setVideosService(VideosService videosService) {
